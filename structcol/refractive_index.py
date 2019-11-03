e8040eeacb45bbc4e61c391127104b344fff76bf
victoriahwang89 <vhwang@g.harvard.edu>
2019-11-03 09:54:26 -0500

    added brookite and anatase indices but need to confirm they are reliable

--- structcol/refractive_index.py
+++ structcol/refractive_index.py
@@ -134,7 +134,25 @@ n_dict = {
                                  
     # the w/w is a crude hack to make the function output an array when the
     # input is an array
-    'vacuum': lambda w: Quantity('1.0')*w/w
+    'vacuum': lambda w: Quantity('1.0')*w/w,
+    
+#------------------------------------------------------------------------------
+    # FROM KEITH
+								
+    # brookite TiO2 from Radhakrishnan. "The Optical Properties of titanium
+    # dioxide". Proceedings of the Indian Academy of Sciences-Mathematical Sciences
+    #March 1982, 35:117. Note that this is for n_alpha. However, n_alpha is 
+    #almost identical to n_beta, which in turn is very similar to rutile. However
+    #n_gama is a bit different, but is not considered
+    # data for rutile TiO2, ordinary ray, 0.43-0.71 micrometers
+    'brookite': lambda w: np.sqrt(2.9858 + 2.1036*w*w/(w*w - Quantity('0.287**2 um^2'))-Quantity('0.18 um^-2')*w*w+1.),
+								
+	# anatase TiO2 from Wang et al. Think Solid Films. 405, 2002, 50-54
+    # measured from 500-1700 nm
+    'anatase': lambda w: 2.1526 +
+                                Quantity('4.1155e-2 um^2')/(w*w)+
+                                Quantity('2.1798e-3 um^4')/(w*w*w*w)
+#------------------------------------------------------------------------------
 }
 
 @ureg.check(None, '[length]')   # ensures wavelen has units of length
